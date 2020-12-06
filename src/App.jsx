1e34f7f22bf539ab8aac6cd97670730f32707f85
Rahi Khan <rahikhan360@gmail.com>
2020-12-06 11:06:00 +0600

    error page 🚫

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Wrapper from "./components/Layout/Wrapper";
 const Home = lazy(() => import("./pages/Home"));
 const LeadTeam = lazy(() => import("./pages/LeadTeam"));
 const Story = lazy(() => import("./pages/Story"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 const App = () => (
     <Wrapper>
         <Route exact path="/" component={Home} />
         <Route exact path="/leadteam" component={LeadTeam} />
         <Route exact path="/story" component={Story} />
+        <Route component={NotFound} />
     </Wrapper>
 );
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NotFound = () => (
+    <div className="notFound d-flex flex-column justify-content-center align-items-center">
+        <div className="errorHeading">404 ERROR</div>
+        <div className="oops">OOPS!</div>
+        <Link to="/">
+            <Button variant="outline-primary" className="backToHomeBtn">
+                Go Back Home
+            </Button>
+        </Link>
+    </div>
+);
+
+export default NotFound;
